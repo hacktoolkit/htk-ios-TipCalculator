782303901269f3b2b315ab2095768eaed4dd5dd0
Jonathan Tsai <jontsai@jontsai.com>
2014-08-21 05:35:26 -0700

    initial import for Tip Calculator iOS app written in Swift

---
+++ README.md
@@ -0,0 +1,4 @@
+htk-ios-TipCalculator
+=====================
+
+This is the pre-selection homework activity for http://codepath.com/iosbootcamp
---
+++ tips/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Jonathan Tsai on 8/21/14.
+//  Copyright (c) 2014 Hacktoolkit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+
+}
+
